b45cac8c1e2fc1c019ccd34787d5c8636d985ba6
James Coyle <jamescoyle1989@gmail.com>
2021-08-21 15:20:31 -0400

    Added recently added classes to index

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
 
 import ButtonInput, { ButtonEvent, ButtonEventData } from './ButtonInput';
 import Clock from './Clock';
+import EventSubscriber from './EventSubscriber';
+import Keyboard from './Keyboard';
+import Metronome from './Metronome';
 import MidiAccess from './MidiAccess';
 import MidiOut from './MidiOut';
 import Note from './Note';
 import PropertyTracker from './PropertyTracker';
 import ShimiEvent, { ShimiEventData } from './ShimiEvent';
+import TimeSig, { TimeSigDivision } from './TimeSig';
 
 export default {
     ButtonInput, ButtonEvent, ButtonEventData,
     Clock,
+    EventSubscriber,
+    Keyboard,
+    Metronome,
     MidiAccess,
     MidiOut,
     Note,
     PropertyTracker,
-    ShimiEvent, ShimiEventData
+    ShimiEvent, ShimiEventData,
+    TimeSig, TimeSigDivision
 }
