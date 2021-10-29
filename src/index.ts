9d1dda3ca317c9daa413a09263c073fcc6be4a4a
James Coyle <jamescoyle1989@gmail.com>
2021-10-29 19:40:04 -0400

    Exporting more classes

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,18 @@ import ButtonInput, { ButtonEvent, ButtonEventData } from './ButtonInput';
 import { Clip, ClipBend, ClipCC, ClipNote } from './Clip';
 import ClipPlayer from './ClipPlayer';
 import Clock from './Clock';
+import Cue from './Cue';
 import EventSubscriber from './EventSubscriber';
 import Flexinome from './Flexinome';
 import Keyboard from './Keyboard';
-import * as messages from './MidiMessages';
 import Metronome from './Metronome';
 import MidiAccess from './MidiAccess';
+import * as messages from './MidiMessages';
 import MidiOut from './MidiOut';
 import Note from './Note';
 import PropertyTracker from './PropertyTracker';
+import Scale from './Scale';
+import ScaleTemplate from './ScaleTemplate';
 import ShimiEvent, { ShimiEventData } from './ShimiEvent';
 import TimeSig, { TimeSigDivision } from './TimeSig';
 
@@ -20,6 +23,7 @@ export {
     Clip, ClipBend, ClipCC, ClipNote,
     ClipPlayer,
     Clock,
+    Cue,
     EventSubscriber,
     Flexinome,
     Keyboard,
@@ -29,6 +33,8 @@ export {
     MidiOut,
     Note,
     PropertyTracker,
+    Scale,
+    ScaleTemplate,
     ShimiEvent, ShimiEventData,
     TimeSig, TimeSigDivision
 }
