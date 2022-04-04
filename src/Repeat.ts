4acb92b0cb0fdad00d01956f5cab3ebad986a62d
James Coyle <jamescoyle1989@gmail.com>
2022-04-04 18:48:03 -0400

    Added Repeat classes

---
+++ src/Repeat.ts
@@ -0,0 +1,147 @@
+'use strict';
+
+import { IClockChild } from './Clock';
+import { IMetronome } from './Metronome';
+
+
+class RepeatBase<TArgs> implements IClockChild {
+    /** Provides a way of identifying cues so they can be retrieved later */
+    get ref(): string { return this._ref; }
+    /** Provides a way of identifying cues so they can be retrieved later */
+    set ref(value: string) { this._ref = value; }
+    private _ref: string;
+
+    get finished(): boolean { return this._finished; }
+    private _finished: boolean = false;
+
+    get action(): (args: TArgs) => void { return this._action; }
+    private _action: (args: TArgs) => void;
+
+    constructor(action: (args: TArgs) => void) {
+        this._action = action;
+    }
+
+    update(deltaMs: number): void {
+    }
+
+    finish(): void {
+        this._finished = true;
+    }
+}
+
+
+export class RepeatArgs {
+    get ms() { return this._ms; }
+    private _ms: number;
+
+    constructor(ms: number) {
+        this._ms = ms;
+    }
+}
+
+
+export class ConditionalRepeat extends RepeatBase<RepeatArgs> {
+    get condition(): () => boolean { return this._condition; }
+    private _condition: () => boolean;
+
+    private _countPassed: number = 0;
+
+    constructor(condition: () => boolean, action: (args: RepeatArgs) => void) {
+        super(action);
+        this._condition = condition;
+    }
+
+    update(deltaMs: number): void {
+        this._countPassed += deltaMs;
+        if (this.condition())
+            this.finish();
+        else
+            this.action(new RepeatArgs(this._countPassed));
+    }
+}
+
+
+export class FiniteRepeatArgs extends RepeatArgs {
+    get percent() { return this._percent; }
+    private _percent: number;
+
+    constructor(ms: number, percent: number) {
+        super(ms);
+        this._percent = percent;
+    }
+}
+
+
+export class MsRepeat extends RepeatBase<FiniteRepeatArgs> {
+    get msCount(): number { return this._msCount; }
+    private _msCount: number;
+
+    private _countPassed: number = 0;
+
+    constructor(msCount: number, action: (args: FiniteRepeatArgs) => void) {
+        super(action);
+        this._msCount = msCount;
+    }
+
+    update(deltaMs: number): void {
+        this._countPassed += deltaMs;
+        if (this._countPassed >= this.msCount)
+            this.finish();
+        else
+            this.action(new FiniteRepeatArgs(this._countPassed, this._countPassed / this.msCount));
+    }
+}
+
+
+export class BeatRepeatArgs extends FiniteRepeatArgs {
+    get beat() { return this._beat; }
+    private _beat: number;
+
+    constructor(ms: number, beat: number, percent: number) {
+        super(ms, percent);
+        this._beat = beat;
+    }
+}
+
+
+export class BeatRepeat extends RepeatBase<BeatRepeatArgs> {
+    get metronome(): IMetronome { return this._metronome; }
+    private _metronome: IMetronome;
+
+    get beatCount(): number { return this._beatCount; }
+    private _beatCount: number;
+
+    private _beatsPassed: number = 0;
+
+    private _msPassed: number = 0;
+
+    constructor(metronome: IMetronome, beatCount: number, action: (args: BeatRepeatArgs) => void) {
+        super(action);
+        this._metronome = metronome;
+        this._beatCount = beatCount;
+    }
+
+    update(deltaMs: number): void {
+        this._msPassed += deltaMs;
+        this._beatsPassed += this.metronome.totalBeatTracker.value - this.metronome.totalBeatTracker.oldValue;
+        if (this._beatsPassed >= this.beatCount)
+            this.finish();
+        else
+            this.action(new BeatRepeatArgs(this._msPassed, this._beatsPassed, this._beatsPassed / this.beatCount));
+    }
+}
+
+
+export default class Repeat {
+    static until(condition: () => boolean, action: (args: RepeatArgs) => void): ConditionalRepeat {
+        return new ConditionalRepeat(condition, action);
+    }
+
+    static forMs(msCount: number, action: (args: FiniteRepeatArgs) => void): MsRepeat {
+        return new MsRepeat(msCount, action);
+    }
+
+    static forBeats(metronome: IMetronome, beatCount: number, action: (args: BeatRepeatArgs) => void): BeatRepeat {
+        return new BeatRepeat(metronome, beatCount, action);
+    }
+}
---
+++ test/Repeat.test.ts
@@ -0,0 +1,124 @@
+import { suite, test } from '@testdeck/mocha';
+import { expect } from 'chai';
+import Metronome from '../src/Metronome';
+import Repeat from '../src/Repeat';
+
+
+@suite class RepeatTests {
+    @test 'MsRepeat executes action multiple times'() {
+        let updateValue: number = 0;
+        const repeat = Repeat.forMs(100, () => updateValue++);
+        repeat.update(10);
+        expect(updateValue).to.equal(1);
+        repeat.update(10);
+        expect(updateValue).to.equal(2);
+    }
+
+    @test 'MsRepeat finishes after msCount has passed'() {
+        let updateValue: number = 0;
+        const repeat = Repeat.forMs(99, () => updateValue++);
+        repeat.update(50);
+        expect(updateValue).to.equal(1);
+        expect(repeat.finished).to.be.false;
+        repeat.update(50);
+        expect(updateValue).to.equal(1);
+        expect(repeat.finished).to.be.true;
+    }
+
+    @test 'ConditionalRepeat executes action until condition is met'() {
+        let trigger: boolean = false;
+        let updateValue: number = 0;
+        const repeat = Repeat.until(() => trigger, () => updateValue++);
+        repeat.update(100);
+        expect(updateValue).to.equal(1);
+        repeat.update(100);
+        expect(updateValue).to.equal(2);
+        expect(repeat.finished).to.be.false;
+        trigger = true;
+        repeat.update(100);
+        expect(repeat.finished).to.be.true;
+    }
+
+    @test 'BeatRepeat executes action until beatCount has passed'() {
+        const metronome = new Metronome(60);
+        let updateValue: number = 0;
+        const repeat = Repeat.forBeats(metronome, 1, () => updateValue++);
+
+        metronome.update(900);
+        repeat.update(900);
+        expect(updateValue).to.equal(1);
+        expect(repeat.finished).to.be.false;
+        metronome.update(101);
+        repeat.update(101);
+        expect(updateValue).to.equal(1);
+        expect(repeat.finished).to.be.true;
+    }
+
+    @test 'ConditionalRepeat includes ms action arg'() {
+        let trigger: boolean = false;
+        let updateValue: number = 0;
+        const repeat = Repeat.until(() => trigger, args => updateValue = args.ms);
+
+        repeat.update(95);
+        expect(updateValue).to.equal(95);
+        repeat.update(107);
+        expect(updateValue).to.equal(202);
+    }
+
+    @test 'MsRepeat includes ms action arg'() {
+        let updateValue: number = 0;
+        const repeat = Repeat.forMs(500, args => updateValue = args.ms);
+
+        repeat.update(95);
+        expect(updateValue).to.equal(95);
+        repeat.update(107);
+        expect(updateValue).to.equal(202);
+    }
+
+    @test 'MsRepeat includes percent action arg'() {
+        let updateValue: number = 0;
+        const repeat = Repeat.forMs(400, args => updateValue = args.percent);
+
+        repeat.update(100);
+        expect(updateValue).to.equal(0.25);
+        repeat.update(150);
+        expect(updateValue).to.equal(0.625);
+    }
+
+    @test 'BeatRepeat includes ms action arg'() {
+        const metronome = new Metronome(60);
+        let updateValue: number = 0;
+        const repeat = Repeat.forBeats(metronome, 4, args => updateValue = args.ms);
+
+        repeat.update(95);
+        expect(updateValue).to.equal(95);
+        repeat.update(107);
+        expect(updateValue).to.equal(202);
+    }
+
+    @test 'BeatRepeat includes percent action arg'() {
+        const metronome = new Metronome(60);
+        let updateValue: number = 0;
+        const repeat = Repeat.forBeats(metronome, 4, args => updateValue = args.percent);
+
+        metronome.update(1000);
+        repeat.update(1000);
+        expect(updateValue).to.equal(0.25);
+        metronome.update(1500);
+        repeat.update(1500);
+        expect(updateValue).to.equal(0.625);
+    }
+
+    @test 'BeatRepeat includes beat action arg'() {
+        const metronome = new Metronome(60);
+        let updateValue: number = 0;
+        const repeat = Repeat.forBeats(metronome, 4, args => updateValue = args.beat);
+
+        metronome.update(1000);
+        repeat.update(1000);
+        expect(updateValue).to.equal(1);
+        metronome.update(1500);
+        repeat.update(1500);
+        expect(updateValue).to.equal(2.5);
+    }
+}
