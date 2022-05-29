741fee956100781a3bb1d35e197943734ca34811
James Coyle <jamescoyle1989@gmail.com>
2022-05-28 20:04:41 -0400

    Improved ability to define custom WebSynthChannels

--- src/WebSynth.ts
+++ src/WebSynth.ts
@@ -7,35 +7,67 @@ import Note from './Note';
 import * as messages from './MidiMessages';
 
 
-class WebSynthChannel {
+/** An interface for defining a how notes are played for a specific MIDI channel */
+export interface IWebSynthChannel {
+    createOscillators(note: Note, frequency: number): Array<OscillatorNode>;
+}
+
+/** The default implementation of IWebSynthChannel */
+export class WebSynthChannel {
     audioContext: AudioContext;
     gain: GainNode;
+    type: any;
 
-    constructor(audioContext: AudioContext) {
+    constructor(audioContext: AudioContext, type: any) {
         this.audioContext = audioContext;
+        this.type = type;
         this.gain = audioContext.createGain();
         this.gain.gain.value = 0.1;
         this.gain.connect(this.audioContext.destination);
     }
 
-    createOscillators(note, frequency): Array<OscillatorNode> {
+    /** Gets passed in a note, plus the frequency of that note. Returns an array of oscillators to play it */
+    createOscillators(note: Note, frequency: number): Array<OscillatorNode> {
         const oscillator = this.audioContext.createOscillator();
-        oscillator.type = 'sawtooth';
+        oscillator.type = this.type;
         oscillator.connect(this.gain);
-        oscillator.frequency.value = 440;
+        oscillator.frequency.value = frequency;
         oscillator.start();
         return [oscillator];
     };
 }
 
 
+/** Can be used in place of MidiOut as a simple synth to play notes in the browser, rather than sending MIDI data externally */
 export default class WebSynth implements IMidiOut, IClockChild {
-    channels: Array<WebSynthChannel> = [];
+    private _audioContext: AudioContext;
+    get audioContext() { return this._audioContext; }
+    
+    private _channels: Array<IWebSynthChannel> = [
+        null,null,null,null,
+        null,null,null,null,
+        null,null,null,null,
+        null,null,null,null
+    ];
 
     constructor(audioContext: AudioContext) {
+        this._audioContext = audioContext;
+    }
+
+    /** Define an IWebSynthChannel object to use for a specific MIDI channel number */
+    withChannel(channelNumber: number, channelImplementation: IWebSynthChannel): WebSynth {
+        this._channels[channelNumber] = channelImplementation;
+        return this;
+    }
+
+    /** Defines all undefined channels with the default IWebSynthChannel implementation */
+    withDefaultChannels(): WebSynth {
+        const types = ['sine', 'square', 'sawtooth', 'triangle'];
         for (let i = 0; i < 16; i++) {
-            this.channels.push(new WebSynthChannel(audioContext));
+            if (!this._channels[i])
+                this._channels[i] = new WebSynthChannel(this.audioContext, types[i % 4]);
         }
+        return this;
     }
 
 
@@ -164,7 +196,7 @@ export default class WebSynth implements IMidiOut, IClockChild {
     }
 
     private _createOscillator(note: Note) {
-        const channel = this.channels[note.channel];
+        const channel = this._channels[note.channel];
         note['oscillators'] = channel.createOscillators(note, this._pitchToFrequency(note.pitch));
     }
 }
--- test/WebSynth.test.ts
+++ test/WebSynth.test.ts
@@ -1,7 +1,7 @@
 import { suite, test } from '@testdeck/mocha';
 import { expect } from 'chai';
 import { Note } from '../src';
-import WebSynth from '../src/WebSynth';
+import WebSynth, {WebSynthChannel} from '../src/WebSynth';
 
 
 class DummyAudioContext {
@@ -31,7 +31,7 @@ class DummyAudioContext {
 @suite class WebSynthTests {
     @test 'addNote adds to note collection'() {
         const audioContext: any = new DummyAudioContext();
-        const synth = new WebSynth(audioContext);
+        const synth = new WebSynth(audioContext).withDefaultChannels();
         expect(synth.notes.length).to.equal(0);
         synth.addNote(new Note(50, 89, 3));
         expect(synth.notes.length).to.equal(1);
@@ -39,7 +39,7 @@ class DummyAudioContext {
 
     @test 'addNote starts a new note sound if the note is on'() {
         const audioContext: any = new DummyAudioContext();
-        const synth = new WebSynth(audioContext);
+        const synth = new WebSynth(audioContext).withDefaultChannels();
         const note = new Note(70, 38, 2);
         expect(note.on).to.be.true;
         expect(note['oscillators']).to.be.undefined;
@@ -49,7 +49,7 @@ class DummyAudioContext {
 
     @test 'oscillator stops on update if note is turned off'() {
         const audioContext: any = new DummyAudioContext();
-        const synth = new WebSynth(audioContext);
+        const synth = new WebSynth(audioContext).withDefaultChannels();
         const note = new Note(57, 89, 8);
         synth.addNote(note);
         expect(note['oscillators']).to.be.not.undefined;
@@ -60,15 +60,40 @@ class DummyAudioContext {
 
     @test 'pitchToFrequency returns correct value'() {
         const audioContext: any = new DummyAudioContext();
-        const synth = new WebSynth(audioContext);
+        const synth = new WebSynth(audioContext).withDefaultChannels();
         expect(synth['_pitchToFrequency'](69)).to.equal(440);
     }
 
     @test 'stopNotes ends notes'() {
         const audioContext: any = new DummyAudioContext();
-        const synth = new WebSynth(audioContext);
+        const synth = new WebSynth(audioContext).withDefaultChannels();
         synth.addNote(new Note(47, 28, 9));
         synth.stopNotes(n => true);
         expect(synth.notes.length).to.equal(0);
+    }
+
+    @test 'withChannel populates a specific channel'() {
+        const audioContext: any = new DummyAudioContext();
+        const synth = new WebSynth(audioContext);
+        expect(synth['_channels'][2]).to.be.null;
+        synth.withChannel(2, new WebSynthChannel(synth.audioContext, 'sawtooth'));
+        expect(synth['_channels'][2]).to.be.not.null;
+        expect(synth['_channels'][1]).to.be.null;
+        expect(synth['_channels'][3]).to.be.null;
+    }
+
+    @test 'withDefaultChannels populates all channels'() {
+        const audioContext: any = new DummyAudioContext();
+        const synth = new WebSynth(audioContext).withDefaultChannels();
+        for (let i = 0; i < 16; i++)
+            expect(synth['_channels'][i]).to.be.not.null;
+    }
+
+    @test 'withDefaultChannels doesnt overwrite already set channels'() {
+        const audioContext: any = new DummyAudioContext();
+        const synth = new WebSynth(audioContext);
+        const channel = new WebSynthChannel(synth.audioContext, 'square');
+        synth.withChannel(5, channel).withDefaultChannels();
+        expect(synth['_channels'][5]).to.equal(channel);
     }
 }
