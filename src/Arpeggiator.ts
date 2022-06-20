d1dc8a9470cb7e64b296a7b6775f41321fc5969b
James Coyle <jamescoyle1989@gmail.com>
2022-06-19 23:07:53 -0400

    Added Arpeggiator documentation.

--- src/Arpeggiator.ts
+++ src/Arpeggiator.ts
@@ -10,70 +10,73 @@ import PropertyTracker from './PropertyTracker';
 
 
 /**
+ * The Arpeggiator is much like to Arpeggios, what the Clip Player is to Clips.
+ * 
+ * The Arpeggiator should be added to a clock to receive regular updates. It should hold a reference to a metronome for beat timings. It should hold a reference to a MIDI out for it to send notes to. It should hold a reference to an arpeggio, which defines the shape that it will play. And it should hold a reference which defines what chord it should play the arpeggio shape around.
+ * 
  * @category Chords & Scales
  */
 export default class Arpeggiator implements IClockChild {
     /** Which arpeggio to play */
     get arpeggio(): Arpeggio { return this._arpeggio; }
-    /** Which arpeggio to play */
     set arpeggio(value: Arpeggio) { this._arpeggio = value; }
     private _arpeggio: Arpeggio;
 
     /** Which chord to play */
     get chord(): Chord { return this._chord; }
-    /** Which chord to play */
     set chord(value: Chord) { this._chord = value; }
     private _chord: Chord;
 
-    /** Which channel to play the arpeggio on */
+    /** The default channel for arpeggiated notes to be played on. */
     get channel(): number { return this._channel; }
-    /** Which channel to play the arpeggio on */
     set channel(value: number) { this._channel = value; }
     private _channel: number = 0;
 
-    /** Provides a way of identifying players so they can be retrieved later */
+    /** Provides a way of identifying arpeggiators so they can easily be retrieved later */
     get ref(): string { return this._ref; }
-    /** Provides a way of identifying players so they can be retrieved later */
     set ref(value: string) { this._ref = value; }
     private _ref: string;
 
-    /** The metronome which the player uses for tracking passed beats */
+    /** The metronome which the player uses for tracking beat timings */
     get metronome(): IMetronome { return this._metronome; }
-    /** The metronome which the player uses for tracking passed beats */
     set metronome(value: IMetronome) { this._metronome = value; }
     private _metronome: IMetronome;
 
     /** The MidiOut which data from the arpeggio gets played on */
     get midiOut(): IMidiOut { return this._midiOut; }
-    /** The MidiOut which data from the arpeggio gets played on */
     set midiOut(value: IMidiOut) { this._midiOut = value; }
     private _midiOut: IMidiOut;
 
-    /** How many beats in the arpeggio to play for every beat that passes in actual time */
+    /** How many beats in the arpeggio to play for every beat that passes in actual time. For example: 1.5 means the arpeggio is played 1.5 times faster than it would normally. */
     get speed(): number { return this._speed; }
-    /** How many beats in the arpeggio to play for every beat that passes in actual time */
     set speed(value: number) { this._speed = value; }
     private _speed: number = 1;
 
     /** Which beat of the arpeggio playback is at */
     get beat(): number { return this._beatTracker.value; }
-    /** Which beat of the arpeggio playback is at */
     set beat(value: number) { this._beatTracker.value = value; }
     get beatTracker(): PropertyTracker<number> { return this._beatTracker; }
     private _beatTracker: PropertyTracker<number> = new PropertyTracker(0);
 
-    /** If not running, then the player won't do anything with each update cycle */
+    /** If not running, then the arpeggiator won't do anything with each update cycle */
     get running(): boolean { return this._running; }
-    /** If not running, then the player won't do anything with each update cycle */
     set running(value: boolean) { this._running = value; }
     private _running: boolean = true;
 
-    /** Allows attaching custom logic to modify each note produced by the arpeggiator */
+    /**
+     * Allows attaching custom logic to modify each note produced by the arpeggiator.
+     * 
+     * For example:
+     * ```
+     * const arp = new Arpeggiator(arpeggio, metronome, midiOut);
+     * arp.noteModifier = note => note.velocity = Math.floor(Math.random() * 128);  //Give each arpeggiated note a random velocity
+     * ```
+     */
     get noteModifier(): ((note: Note) => void) { return this._noteModifier; }
-    /** Allows attaching custom logic to modify each note produced by the arpeggiator */
     set noteModifier(value: ((note: Note) => void)) { this._noteModifier = value; }
     private _noteModifier: (note: Note) => void;
 
+    /** Signifies whether the arpeggiator has stopped. */
     get finished(): boolean { return this._finished; }
     private _finished: boolean = false;
 
@@ -85,6 +88,11 @@ export default class Arpeggiator implements IClockChild {
         this.midiOut = midiOut;
     }
 
+    /**
+     * This method is intended to be called by a clock to provide regular updates. It should not be called by consumers of the library.
+     * @param deltaMs How many milliseconds have passed since the last update cycle.
+     * @returns 
+     */
     update(deltaMs: number): void {
         if (!this.running || !this.arpeggio || !this.metronome || !this.midiOut)
             return;
@@ -119,6 +127,7 @@ export default class Arpeggiator implements IClockChild {
         }
     }
 
+    /** Calling this tells the arpeggiator to stop whatever it's doing and that it will no longer be used. */
     finish(): void {
         this._finished = true;
         this._endAllNotes();
